f408cdd1e88cb6a2bd6ef5eeebc355f21a912184
SamaadTurner <samaad.turner@gmail.com>
2023-10-24 22:58:38 -0700

    challenge 27

---
+++ javascript/401/merge-sort/README.md
@@ -0,0 +1,11 @@
+# Merge Sort
+- an algorithm that splits an arrray in half and sorts values lowest to highest in a new array.  
+
+# Approach and Efficiency:  
+Time: O(nlog(n)) 
+space: O(n)
+
+*Shoutout to chatgpt*
+
+# Whiteboard  
+![whiteboard](<Screenshot 2023-10-24 at 10.49.36 PM.png>)
---
+++ javascript/401/merge-sort/__tests__/merge.sort.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { mergeSort } = require('../index.js');
+
+describe('insertionSort', () => {
+
+  test('[8,4,23,42,16,15]', () => {
+    let array = [8,4,23,42,16,15];
+    expect(mergeSort(array)).toEqual([4,8,15,16,23,42]);
+  });
+
+  test('[20,18,12,8,5,-2]', () => {
+    let array = [20,18,12,8,5,-2];
+    expect(mergeSort(array)).toEqual([-2,5,8,12,18,20]);
+  });
+
+  test('[5,12,7,5,5,7]', () => {
+    let array = [5,12,7,5,5,7];
+    expect(mergeSort(array)).toEqual([5,5,5,7,7,12]);
+  });
+
+  test('[2,3,5,7,13,11]', () => {
+    let array = [2,3,5,7,13,11];
+    expect(mergeSort(array)).toEqual([2,3,5,7,11,13]);
+  });
+
+});
